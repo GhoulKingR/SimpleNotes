2474f35b9e331045b9e6e0d611389b8e400d56ed
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-08-12 09:14:39 +0100

    Initial Commit

---
+++ SimpleNotes/SimpleNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNotesApp.swift
+//  SimpleNotes
+//
+//  Created by GhoulKingR on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
